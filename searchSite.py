34bd40c5d2ae033b402b8575a01d5a0c8cfde629
Gazeux33 <gazeux33000@gmail.com>
2023-08-12 18:27:31 +0200

    first commit

---
+++ searchSite.py
@@ -0,0 +1,34 @@
+import random
+import requests
+import json
+import os
+
+
+PATH_NAME = os.getcwd()
+search = True
+
+
+with open(os.path.join(PATH_NAME,"data/name.json"),"r") as file:
+    data_name = json.load(file)
+    
+
+with open(os.path.join(PATH_NAME,"data/word.json"),"r") as file:
+    data_words = json.load(file)
+    
+data = data_name + data_words
+
+
+
+while search == True:
+    name = random.choice(data)
+    url = f"https://www.{name}.com/"
+    try:
+        request = requests.get(url)
+
+        if request.status_code == 200:
+            print(f"Site trouvé:{url}")
+            search = False
+    except:
+        print(f"echec:{url}")
+
+

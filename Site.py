bf310002b6bb00e80833e537e2a92194df1944ca
Gazeux33 <gazeux33000@gmail.com>
2023-08-13 13:43:56 +0200

    up

--- Site.py
+++ Site.py
@@ -1,7 +1,7 @@
 import random
 import requests
 import json
-import os 
+
 
 
 
@@ -17,8 +17,7 @@ class Site:
         self.data_site = self.get_data("site.json")
         
         self.search_site()
-        
-        
+          
     def get_data(self,filename:str):
         with open(f"data/{filename}","r") as file:
             data = json.load(file)
@@ -28,16 +27,13 @@ class Site:
         self.data_site.append(data)
         with open(f"data/{filename}","w") as file:
             json.dump(self.data_site,file,indent=2)
-            
-        
+              
     def get_url(self):
         name = random.choice(self.data)
         url = f"https://www.{name}.com/"
         return url
-          
-          
+              
     def search_site(self):
-        
         while self.search == True:
             self.url = self.get_url()
             try:
